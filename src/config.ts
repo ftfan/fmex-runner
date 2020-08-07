88c4711a3cb6e7cb0b452c24d3ef7862046ff518
imconfig <982748666@qq.com>
2020-08-07 11:59:20 +0800

    多个选择

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const UserConfig = {
+const UserConfig = {
   alinode: {
     appid: '',
     secret: '',
@@ -13,5 +13,15 @@ try {
   const RealConfig = require('./user.config');
   Object.assign(UserConfig, RealConfig);
 } catch (e) {
+  if (process.env.NODE_ENV !== 'local') {
+    try {
+      const RealConfig = require('../src/user.config');
+      Object.assign(UserConfig, RealConfig);
+    } catch (e) {
+      // 用户没有配置
+    }
+  }
   // 用户没有配置
 }
+
+export { UserConfig };
